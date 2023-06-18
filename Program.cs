1a34e057d2294dd1d8c78ca9c1773c9d31369258
ILDUS <ildus6696@gmail.com>
2023-06-18 10:41:36 +0300

    Reshena Zadacha 61

--- Program.cs
+++ Program.cs
@@ -221,3 +221,39 @@
 // int[,,] ThreeMatrix = new int[size[0], size[1], size[2]];
 // InputMatrix(ThreeMatrix);
 // PrintMatrix(ThreeMatrix);
+
+// Задача 61(Дополнительная): Вывести первые N строк треугольника Паскаля. 
+// Сделать вывод в виде равнобедренного треугольника
+int factorial(int n)
+{
+    int i, x = 1;
+    for (i = 1; i <= n; i++)
+    {
+        x *= i;
+    }
+    return x;
+}
+
+
+void ReleaseTask(int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= (n - i); j++)
+        {
+            Console.Write(" ");
+        }
+        for (int j = 0; j <= i; j++)
+        {
+            Console.Write(" ");
+            Console.Write(factorial(i) / (factorial(j) * factorial(i - j)));
+        }
+        Console.WriteLine();
+    }
+}
+
+
+Console.Clear();
+Console.Write("Введите количество строк треугольника Паскаля: ");
+int n = Convert.ToInt32(Console.ReadLine());
+ReleaseTask(n);
